30fb479d088360bf1e4d2c37b27c2b72ec3b8e6f
mansi_dxtera <mansi@dxtera.org>
2021-06-30 14:15:21 +0530

    Initial Commit

---
+++ helper_evaluation.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import random
+import torch
+
+def set_all_seeds(seed):
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
