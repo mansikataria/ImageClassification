04b826eefd3b6e46a74b5bff0cd24e683c08e5ab
Mansi <11619444+mansikataria@users.noreply.github.com>
2021-07-01 14:21:43 +0530

    Update helper_dataloaders.py

--- helper_dataloaders.py
+++ helper_dataloaders.py
@@ -5,6 +5,27 @@ from torch.utils.data import DataLoader
 from torch.utils.data import SubsetRandomSampler
 from torchvision import transforms
 
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Parameters:
+        ------------
+        tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        
+        Returns:
+        ------------
+        Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
 def get_dataloaders_cifar10(batch_size, num_workers=0,
                             validation_fraction=None,
                             train_transforms=None,
